2bea5617248d0fe9a7f5d0bc65efbf2aedb4dbba
Overfirst <overfirst@inbox.ru>
2021-07-28 12:41:41 +0300

    Created sheduler month view component (only layout)

--- src/app/shared/modules/sheduler.module.ts
+++ src/app/shared/modules/sheduler.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from './shared.module';
 import { ShedulerComponent } from '../components/sheduler/sheduler.component';
+import { ShedulerMonthViewComponent } from '../components/sheduler/views/sheduler-month-view/sheduler-month-view.component';
 
 @NgModule({
   imports: [SharedModule],
-  declarations: [ShedulerComponent],
-  exports: [ShedulerComponent]
+  declarations: [ShedulerComponent, ShedulerMonthViewComponent],
+  exports: [ShedulerComponent, ShedulerMonthViewComponent]
 })
 export class ShedulerModule {}
