055559ce97ef7c34d82c676b36d35b983bcf468c
Overfirst <overfirst@inbox.ru>
2021-08-03 11:03:20 +0300

    Colors for all events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,54 +13,63 @@ export class AppComponent {
       name: 'Create sheduler application',
       start: new Date('2021-08-06 12:00'),
       end: new Date('2021-08-13 13:00'),
+      color: '#b3c3ff'
     },
     {
       id: 2,
       name: 'Test task',
       start: new Date('2021-08-05 14:00'),
       end: new Date('2021-08-06 17:00'),
+      color: '#f7d061'
     },
     {
       id: 3,
       name: 'Test task 2',
       start: new Date('2021-08-13 18:00'),
       end: new Date('2021-08-15 16:00'),
+      color: '#ffad86'
     },
     {
       id: 4,
       name: 'Test task 3',
       start: new Date('2021-08-15 06:00'),
       end: new Date('2021-08-18 13:00'),
+      color: '#f1a0be'
     },
     {
       id: 5,
       name: 'Test task 4',
       start: new Date('2021-08-17 15:00'),
       end: new Date('2021-08-18 21:00'),
+      color: '#86ffe3'
     },
     {
       id: 6,
       name: 'Test task 5',
       start: new Date('2021-08-21 19:00'),
       end: new Date('2021-08-27 16:00'),
+      color: '#eaff77'
     },
     {
       id: 7,
       name: 'Test task 6',
       start: new Date('2021-08-24 18:00'),
       end: new Date('2021-08-28 21:00'),
+      color: '#f59595'
     },
     {
       id: 8,
       name: 'Test task 7',
       start: new Date('2021-08-25 22:00'),
       end: new Date('2021-08-26 16:00'),
+      color: '#ffa8fc'
     },
     {
       id: 9,
       name: 'Test task 8',
       start: new Date('2021-08-26 19:00'),
       end: new Date('2021-08-27 22:00'),
-    },
+      color: '#655bff'
+    }
   ];
 }
