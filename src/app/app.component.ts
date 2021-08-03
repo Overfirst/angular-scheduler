55aa1fac08781457cb6dcdd5820dcc639722e314
Overfirst <overfirst@inbox.ru>
2021-08-03 09:22:04 +0300

    Updated the data set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,25 @@ export class AppComponent {
       id: 1,
       name: 'Create sheduler application',
       start: new Date('2021-08-06 12:00'),
-      end: new Date('2021-08-12 13:00'),
+      end: new Date('2021-08-13 13:00'),
     },
     {
       id: 2,
       name: 'Test task',
       start: new Date('2021-08-05 04:00'),
-      end: new Date('2021-08-05 13:00'),
+      end: new Date('2021-08-06 13:00'),
+    },
+    {
+      id: 3,
+      name: 'Test task 2',
+      start: new Date('2021-08-13 04:00'),
+      end: new Date('2021-08-15 13:00'),
+    },
+    {
+      id: 4,
+      name: 'Test task 3',
+      start: new Date('2021-08-15 04:00'),
+      end: new Date('2021-08-18 13:00'),
     },
   ];
 }
