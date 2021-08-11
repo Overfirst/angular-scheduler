6aa509dd22dd55bfa86f2e9a339c3b73985f99e9
Overfirst <overfirst@inbox.ru>
2021-08-11 09:23:07 +0300

    The data for the events is placed in a separate file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ShedulerEvent } from './shared/interfaces';
+import { shedulerEvents } from "./sheduler-events";
 
 @Component({
   selector: 'app-root',
@@ -7,76 +8,5 @@ import { ShedulerEvent } from './shared/interfaces';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public events: ShedulerEvent[] = [
-    {
-      id: 1,
-      name: 'Creating a scheduler',
-      start: new Date('2021-08-06 12:00'),
-      end: new Date('2021-08-13 13:00'),
-      color: '#b3c3ff'
-    },
-    {
-      id: 2,
-      name: 'Test task',
-      start: new Date('2021-08-05 14:00'),
-      end: new Date('2021-08-06 17:00'),
-      color: '#f7d061'
-    },
-    {
-      id: 3,
-      name: 'Test task 2',
-      start: new Date('2021-08-13 18:00'),
-      end: new Date('2021-08-15 16:00'),
-      color: '#ffad86'
-    },
-    {
-      id: 4,
-      name: 'Test task 3',
-      start: new Date('2021-08-15 06:00'),
-      end: new Date('2021-08-18 13:00'),
-      color: '#f1a0be'
-    },
-    {
-      id: 5,
-      name: 'Test task 4',
-      start: new Date('2021-08-17 15:00'),
-      end: new Date('2021-08-18 21:00'),
-      color: '#86ffe3'
-    },
-    {
-      id: 6,
-      name: 'Test task 5',
-      start: new Date('2021-08-21 19:00'),
-      end: new Date('2021-08-27 16:00'),
-      color: '#eaff77'
-    },
-    {
-      id: 7,
-      name: 'Test task 6',
-      start: new Date('2021-08-24 18:00'),
-      end: new Date('2021-08-28 21:00'),
-      color: '#f59595'
-    },
-    {
-      id: 8,
-      name: 'Test task 7',
-      start: new Date('2021-08-25 22:00'),
-      end: new Date('2021-08-26 16:00'),
-      color: '#ffa8fc'
-    },
-    {
-      id: 9,
-      name: 'Test task 8',
-      start: new Date('2021-08-26 19:00'),
-      end: new Date('2021-08-27 22:00'),
-      color: '#655bff'
-    },
-    {
-      id: 10,
-      name: 'Test task 9',
-      start: new Date('2021-08-26 21:00'),
-      end: new Date('2021-08-27 16:00'),
-      color: '#c3bcff'
-    }
-  ];
+  public events: ShedulerEvent[] = shedulerEvents;
 }
