171a0752f63b6546b65413fc7644eae77dce89be
Overfirst <overfirst@inbox.ru>
2021-08-11 09:59:04 +0300

    Different text for different views in the absence of events

--- src/app/shared/components/sheduler/sheduler-view-detalization/sheduler-view-detalization.component.ts
+++ src/app/shared/components/sheduler/sheduler-view-detalization/sheduler-view-detalization.component.ts
@@ -1,5 +1,5 @@
-import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
-import { ShedulerEvent, ViewDetalization } from "../../../interfaces";
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {ShedulerEvent, ViewDetalization} from "../../../interfaces";
 
 @Component({
   selector: 'sheduler-view-detalization',
@@ -16,5 +16,25 @@ export class ShedulerViewDetalizationComponent {
 
   public eventClick(event: ShedulerEvent) {
     this.eventClicked.emit(event);
+  }
+
+  public getEmptyText(): string {
+    let text = "There are no events on this ";
+
+    switch (this.view) {
+      case ViewDetalization.Day:
+        text += 'hour';
+        break;
+
+      case ViewDetalization.Week:
+      case ViewDetalization.Month:
+        text += 'day';
+        break;
+
+      case ViewDetalization.Year:
+        text += 'month';
+    }
+
+    return text += '!';
   }
 }
